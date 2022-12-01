91109ce72d69a633c1991cf35a9f7a0b67541289
vashkiv <vashkiv7@gmail.cim>
2022-12-01 19:07:47 +0200

    changed contract

--- contracts/Web3PFP.sol
+++ contracts/Web3PFP.sol
@@ -1,3 +1,4 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -5,30 +6,16 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "hardhat/console.sol";
 
-interface non_standard_IERC20 {
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external ;
 
-    function transfer(
-        address to,
-        uint256 amount
-    ) external ;
-
-    function decimals() external returns (uint8);
-}
-
-contract Web3PFP is ERC721URIStorage, Ownable {
+contract LittleDrawlingsNFT is ERC721URIStorage, Ownable {
     address private _owner;
     address private _wallet;
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter public _tokenIds;
 
-    constructor() ERC721("Web3PFP", "PFP") {
+    constructor() ERC721("LittleDrawlingsNFT", "NFT") {
         _owner = msg.sender;
     }
 
@@ -36,46 +23,22 @@ contract Web3PFP is ERC721URIStorage, Ownable {
     event NFTUpdated(address owner, uint256 tokenID);
     event TransferSent(address _from, uint _amount);
 
-    uint256 public MINT_AMOUNT = 30;
-    uint256 public UPDATE_INFO_AMOUNT = 5;
-    uint256 public UPDATE_PHOTO_AMOUNT = 3;
-    uint256 public PRICE_DECIMALS = 1;
+    uint256 public MINT_AMOUNT = 0.00021 ether;
+    uint256 public UPDATE_INFO_AMOUNT = 0.000025 ether;
+    uint256 public UPDATE_IMAGE_AMOUNT = 0.00000625 ether;
 
-    function changeMintAmount(uint256 value) public onlyOwner isOwner() {
+    function changeAmount(uint256 value) public isOwner() {
         MINT_AMOUNT = value;
     }
 
-    function changeUpdateInfoAmount(uint256 value) public onlyOwner isOwner() {
-        UPDATE_INFO_AMOUNT = value;
-    }
-
-    function changeUpdatePhotoAmount(uint256 value) public onlyOwner isOwner() {
-        UPDATE_PHOTO_AMOUNT = value;
-    }
-
-    function changePriceDecimals(uint8 decimals) public onlyOwner isOwner() {
-        require(decimals > 0, "cannot be less then 1");
-        PRICE_DECIMALS = decimals;
-    }
-
-    function mintNFT(address recipient, string memory tokenURI, uint256 amount, address tokenAddress)
+    function mintNFT(address recipient, string memory tokenURI)
     public
+    payable
     returns (uint256)
     {
         _tokenIds.increment();
-
-        uint decimals = non_standard_IERC20(tokenAddress).decimals();
-        uint price = (MINT_AMOUNT * (10 ** decimals)) / (10 ** PRICE_DECIMALS);
-
-        require(_tokenIds.current() > 0 && _tokenIds.current() < 21000, "Exceeds token supply");
-        require(IERC20(tokenAddress).allowance(msg.sender, address(this)) >= price, "not approved");
-
-        non_standard_IERC20(tokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-
+        console.log('MINT_AMOUNT', MINT_AMOUNT);
+        require(MINT_AMOUNT == (msg.value), "invalid amount");
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
@@ -83,60 +46,22 @@ contract Web3PFP is ERC721URIStorage, Ownable {
         return newItemId;
     }
 
-    function updatePhoto(address tokenAddress, uint256 amount)
-    public
-    returns (uint256)
-    {
-        uint8 decimals = non_standard_IERC20(tokenAddress).decimals();
-        uint price = (UPDATE_PHOTO_AMOUNT * (10 ** decimals)) / (10 ** PRICE_DECIMALS);
-
-        require(IERC20(tokenAddress).allowance(msg.sender, address(this)) >= price, "not approved");
-
-        non_standard_IERC20(tokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-        uint256 itemId = _tokenIds.current();
-        emit NFTUpdated(msg.sender, itemId);
-        return itemId;
+    function updateInfo(uint256 tokenID) public payable {
+        require(msg.value == UPDATE_INFO_AMOUNT, "invalid amount");
+        emit NFTUpdated(msg.sender, tokenID);
     }
 
-    function updateInfo(address tokenAddress, uint256 amount)
-    public
-    returns (uint256)
-    {
-        uint8 decimals = non_standard_IERC20(tokenAddress).decimals();
-        uint price = (UPDATE_INFO_AMOUNT * (10 ** decimals)) / (10 ** PRICE_DECIMALS);
-
-        require(IERC20(tokenAddress).allowance(msg.sender, address(this)) >= price, "not approved");
-
-        non_standard_IERC20(tokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
-        uint256 itemId = _tokenIds.current();
-        emit NFTUpdated(msg.sender, itemId);
-        return itemId;
+    function updateImage(uint256 tokenID) public payable {
+        require(msg.value == UPDATE_IMAGE_AMOUNT, "invalid amount");
+        emit NFTUpdated(msg.sender, tokenID);
     }
 
-    function withdraw(address tokenAddress, uint256 amount) external onlyOwner {
-
-        IERC20 token = IERC20(tokenAddress);
-
-        uint256 erc20balance = token.balanceOf(address(this));
-        require(amount <= erc20balance, "balance is low");
-
-        non_standard_IERC20(tokenAddress).transfer(
-            msg.sender,
-            amount
-        );
-        emit TransferSent(msg.sender, amount);
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
 
     modifier isOwner() {
-        require(_owner == msg.sender, "Web3PFP: not owner");
+        require(_owner == msg.sender, "LittleDrawlings: not owner");
         _;
     }
 
